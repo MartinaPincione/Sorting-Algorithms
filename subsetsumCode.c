2a0d06dfcb46091155305542b00835848f8542bd
MartinaPincione <115095206+MartinaPincione@users.noreply.github.com>
2022-10-06 08:41:57 -0400

    Update subsetsumCode.c

--- subsetsumCode.c
+++ subsetsumCode.c
@@ -1,14 +1,7 @@
 #include "subsetsum.h"
 #include <stdio.h>
 
-/* 
- * This function has 3 inputs and will produce on stdout the list of subsetsum in lexicographic
- * increasing order
- * n : the number of integers in the input set
- * s : the target sum to reach 
- * t : an array of size n with the integers present in the input set.
- * output: none. Only side-effects on the standard output
- */ 
+
 
  void aux(int n, int s, int t[n], int comp_index, int arr_index, int arr[n]){
      if ((s == 0) && (comp_index == n)){
@@ -22,10 +15,9 @@
          return;
      }
      else{
-         //including the comp_num in the arr 
+
          arr[arr_index] = t[comp_index];
          aux(n, s - t[comp_index], t, comp_index + 1, arr_index + 1, arr);
-         //excluding the comp_num in the arr 
          aux(n, s, t, comp_index + 1, arr_index, arr);         
      }
  }
@@ -35,5 +27,4 @@ void subSetSum(int n,int s,int t[n])
     int num_arr=0;
     int arr_index=0;
     aux(n, s, t, num_arr, arr_index, arr);
-   // TODO(Q4.1): Implement this routine
 }
